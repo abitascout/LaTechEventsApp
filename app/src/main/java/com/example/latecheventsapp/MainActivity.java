0475fa5ab36e1362b6a40940d6fad80e7577d42c
sgg014 <sgg014@latech.edu>
2022-08-21 10:53:15 -0500

    Issue with app not launching should be fixed

---
+++ app/src/main/java/com/example/latecheventsapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.latecheventsapp;
+
+import android.os.Bundle;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_login);
+    }
+}
