6bbc1854647d9972fc6a93b43fb42212b530918f
Robert Morton <abitascoutrobert1@gmail.com>
2022-08-23 12:41:20 -0500

    Added more things to the Event.java code

--- app/src/main/java/com/example/latecheventsapp/data/model/Event.java
+++ app/src/main/java/com/example/latecheventsapp/data/model/Event.java
@@ -9,14 +9,18 @@ import java.util.Date;
 public class Event {
     private String Event_Name;
     private String Event_Desc;
+    private String Club_Name;
     private @ServerTimestamp Date timestamp;
+    private String Location;
     private String eventId;
 
-    public Event (String Event_Name, String Event_Desc, Date timestamp, String eventId)
+    public Event (String Event_Name, String Event_Desc, String Club_Name, Date timestamp, String Location, String eventId)
     {
         this.Event_Name = Event_Name;
         this.Event_Desc = Event_Desc;
+        this.Club_Name = Club_Name;
         this.timestamp = timestamp;
+        this.Location = Location;
         this.eventId = eventId;
 
     }
@@ -27,8 +31,14 @@ public class Event {
     public String getEvent_Desc() {return Event_Desc;}
     public void setEvent_Desc() {this.Event_Desc = Event_Desc;}
 
+    public String getClub_Name() {return Club_Name;}
+    public void setClub_Name() {this.Club_Name = Club_Name;}
+
     public Date getTimestamp() {return timestamp;}
     public void setTimestamp() {this.timestamp = timestamp;}
+
+    public String getLocation() {return Location;}
+    public void setLocation() {this.Location = Location;}
 
     public String getEventId() {return eventId;}
     public void setEventId() {this.eventId = eventId;}
