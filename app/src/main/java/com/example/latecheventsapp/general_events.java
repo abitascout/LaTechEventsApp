2f70b27f4a86c5baacb04177c55a5855192990a2
Robert Morton <abitascoutrobert1@gmail.com>
2022-10-26 10:41:44 -0500

    tiddy up some code and made the search bar white with black text

--- app/src/main/java/com/example/latecheventsapp/general_events.java
+++ app/src/main/java/com/example/latecheventsapp/general_events.java
@@ -1,16 +1,18 @@
 package com.example.latecheventsapp;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SearchView;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
@@ -22,29 +24,20 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.latecheventsapp.data.model.Event;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Date;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link general_events#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class general_events extends Fragment implements SwipeRefreshLayout.OnRefreshListener, GenAdapter.GenEventListener {
     private static final String TAG = "General Events";
 
@@ -58,7 +51,7 @@ public class general_events extends Fragment implements SwipeRefreshLayout.OnRef
     private final ArrayList<Event> filteredEvents = new ArrayList<>();
     private ProgressDialog progressDialog;
     private String selectedFilter = "all";
-    private ArrayList<Event> Fevents = new ArrayList<>();
+    private final ArrayList<Event> Fevents = new ArrayList<>();
 
     //widgets
     private RecyclerView recyclerView;
@@ -66,10 +59,7 @@ public class general_events extends Fragment implements SwipeRefreshLayout.OnRef
     private String currnetSearch = "";
     private SearchView searchView;
 
-    public static general_events newInstance() {
-        general_events fragment = new general_events();
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -88,12 +78,12 @@ public class general_events extends Fragment implements SwipeRefreshLayout.OnRef
 
     private void lookSelected(Button parsedButton)
     {
-        parsedButton.setTextColor(ContextCompat.getColor(getContext(), R.color.tech_Red));
-        parsedButton.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.white));
+        parsedButton.setTextColor(ContextCompat.getColor(requireContext(), R.color.tech_Red));
+        parsedButton.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.white));
     }
     private void lookUnSelected(Button parsedButton){
-        parsedButton.setTextColor(ContextCompat.getColor(getContext(), R.color.white));
-        parsedButton.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.tech_Red));
+        parsedButton.setTextColor(ContextCompat.getColor(requireContext(), R.color.white));
+        parsedButton.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.tech_Red));
     }
 
     @Nullable
@@ -103,7 +93,7 @@ public class general_events extends Fragment implements SwipeRefreshLayout.OnRef
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_general_events, container, false);
 
-        Toolbar toolbar = getActivity().findViewById(R.id.toolbar);
+        Toolbar toolbar = requireActivity().findViewById(R.id.toolbar);
         toolbar.setTitle("General Events");
 
         Button allBtn = view.findViewById(R.id.All_button);
@@ -116,52 +106,37 @@ public class general_events extends Fragment implements SwipeRefreshLayout.OnRef
 
 
 
-        allBtn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        AllFilterTapped();
-                        unSelectAllFilters(allBtn,partyBtn,foodBtn,greekBtn,tutorBtn);
-                        lookSelected(allBtn);
-                    }
-                });
+        allBtn.setOnClickListener(v -> {
+            AllFilterTapped();
+            unSelectAllFilters(allBtn,partyBtn,foodBtn,greekBtn,tutorBtn);
+            lookSelected(allBtn);
+        });
 
-        partyBtn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        PartyFilter(v);
-                        unSelectAllFilters(allBtn,partyBtn,foodBtn,greekBtn,tutorBtn);
-                        lookSelected(partyBtn);
-                    }
-                });
+        partyBtn.setOnClickListener(v -> {
+            PartyFilter();
+            unSelectAllFilters(allBtn,partyBtn,foodBtn,greekBtn,tutorBtn);
+            lookSelected(partyBtn);
+        });
 
-        foodBtn.setOnClickListener(new View.OnClickListener() {
-
-                    @Override
-                    public void onClick(View v) {
-                        FoodFilter(v);
-                        unSelectAllFilters(allBtn,partyBtn,foodBtn,greekBtn,tutorBtn);
-                        lookSelected(foodBtn);
-                    }
-                });
+        foodBtn.setOnClickListener(v -> {
+            FoodFilter();
+            unSelectAllFilters(allBtn,partyBtn,foodBtn,greekBtn,tutorBtn);
+            lookSelected(foodBtn);
+        });
 
 
-        greekBtn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        GreekFilter(v);
-                        unSelectAllFilters(allBtn,partyBtn,foodBtn,greekBtn,tutorBtn);
-                        lookSelected(greekBtn);
-                    }
-                });
+        greekBtn.setOnClickListener(v -> {
+            GreekFilter();
+            unSelectAllFilters(allBtn,partyBtn,foodBtn,greekBtn,tutorBtn);
+            lookSelected(greekBtn);
+        });
 
-        tutorBtn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        TutorFilter(v);
-                        unSelectAllFilters(allBtn,partyBtn,foodBtn,greekBtn,tutorBtn);
-                        lookSelected(tutorBtn);
-                    }
-                });
+        tutorBtn.setOnClickListener(v -> {
+            TutorFilter();
+            unSelectAllFilters(allBtn,partyBtn,foodBtn,greekBtn,tutorBtn);
+            lookSelected(tutorBtn);
+        });
+        //for when we decided to filter by clubs
        /* view.findViewById(R.id.Club_button)
                 .setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -170,67 +145,60 @@ public class general_events extends Fragment implements SwipeRefreshLayout.OnRef
                     }
                 });*/
 
-        view = create_handler(view);
-        view = Swiping(view);
+        create_handler(view);
+        Swiping(view);
 
         return view;
     }
 
     // used for the swipe refresh layout
-    private View Swiping(View view) {
+    private void Swiping(View view) {
         swipeRefreshLayout = view.findViewById(R.id.swiper);
 
 
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
+        swipeRefreshLayout.setOnRefreshListener(() -> {
 
-                eventRef.addSnapshotListener((Activity) requireContext(), new EventListener<QuerySnapshot>() {
-                    @Override
-                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-                        if (error != null) {
-                            Log.d("Error", error.toString());
-                        }
-                        for (DocumentChange dc : value.getDocumentChanges()) {
-                            DocumentSnapshot snap = dc.getDocument();
-                            int oldIndex = dc.getOldIndex();
-                            Event temp = snap.toObject(Event.class);
-                            Date date = new Date();
-                            Date eventDate = temp.getStart().toDate();
+            eventRef.addSnapshotListener((Activity) requireContext(), (value, error) -> {
+                if (error != null) {
+                    Log.d("Error", error.toString());
+                }
+                assert value != null;
+                for (DocumentChange dc : value.getDocumentChanges()) {
+                    DocumentSnapshot snap = dc.getDocument();
+                    int oldIndex = dc.getOldIndex();
+                    Event temp = snap.toObject(Event.class);
+                    Date date = new Date();
+                    Date eventDate = temp.getStart().toDate();
 
-                                switch (dc.getType()) {
-                                    case ADDED:
-                                        boolean tempswitch = checking(temp);
-                                        if (!tempswitch)
-                                            if(eventDate.compareTo(date) >= 0)
-                                                eventArrayList.add(temp);
-                                        break;
-                                    case MODIFIED:
-                                        eventArrayList.remove(oldIndex);
+                        switch (dc.getType()) {
+                            case ADDED:
+                                boolean tempswitch = checking(temp);
+                                if (!tempswitch)
+                                    if(eventDate.compareTo(date) >= 0)
                                         eventArrayList.add(temp);
-                                        break;
-                                    case REMOVED:
-                                        Event tempEvent = eventCheck(temp, eventArrayList);
-                                        if (tempEvent != null)
-                                            eventArrayList.remove(tempEvent);
+                                break;
+                            case MODIFIED:
+                                eventArrayList.remove(oldIndex);
+                                eventArrayList.add(temp);
+                                break;
+                            case REMOVED:
+                                Event tempEvent = eventCheck(temp, eventArrayList);
+                                if (tempEvent != null)
+                                    eventArrayList.remove(tempEvent);
 
 
 
-                            }
-                            eventArrayList.sort(Comparator.comparing(e -> e.getStart()));
-
-                        }
                     }
-                });
+                    eventArrayList.sort(Comparator.comparing(Event::getStart));
 
-                GenAdapter refreshAdapter = new GenAdapter(getContext(), eventArrayList, general_events.this);
-                recyclerView.setAdapter(refreshAdapter);
-                swipeRefreshLayout.setRefreshing(false);
+                }
+            });
 
-            }
+            GenAdapter refreshAdapter = new GenAdapter(getContext(), eventArrayList, general_events.this);
+            recyclerView.setAdapter(refreshAdapter);
+            swipeRefreshLayout.setRefreshing(false);
 
         });
-        return view;
     }
     // finds it in the list
     private Event eventCheck(Event event, ArrayList<Event> eventArrayList) {
@@ -260,7 +228,7 @@ public class general_events extends Fragment implements SwipeRefreshLayout.OnRef
     }
 
     // creates the start up view for the app
-    private View create_handler(View view) {
+    private void create_handler(View view) {
 
         progressDialog = new ProgressDialog(getContext());
         progressDialog.setCancelable(false);
@@ -268,35 +236,32 @@ public class general_events extends Fragment implements SwipeRefreshLayout.OnRef
         progressDialog.show();
 
         eventArrayList = new ArrayList<>();
-        adapter = new GenAdapter(getContext(), eventArrayList, this::onEventClick);
+        adapter = new GenAdapter(getContext(), eventArrayList, this);
         testChangeListener();
         recyclerView = view.findViewById(R.id.recycle);
         recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
         recyclerView.setAdapter(adapter);
-        return view;
     }
 
 
     // populates the eventArrayList with the initial data
+    @SuppressLint("NotifyDataSetChanged")
     private void testChangeListener() {
         Date date = new Date();
         Timestamp time = new Timestamp(date);
         eventRef.orderBy("start", Query.Direction.ASCENDING).orderBy("event_Name", Query.Direction.ASCENDING)
                 .whereGreaterThanOrEqualTo("start", time)
                 .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
-                                eventArrayList.add(documentSnapshot.toObject(Event.class));
-                            }
-                            adapter.notifyDataSetChanged();
-                            if (progressDialog.isShowing()) {
-                                progressDialog.dismiss();
-                            }
-
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
+                            eventArrayList.add(documentSnapshot.toObject(Event.class));
                         }
+                        adapter.notifyDataSetChanged();
+                        if (progressDialog.isShowing()) {
+                            progressDialog.dismiss();
+                        }
+
                     }
                 });
     }
@@ -304,6 +269,9 @@ public class general_events extends Fragment implements SwipeRefreshLayout.OnRef
 
     private View genSearch(View view) {
         searchView = view.findViewById(R.id.search_view);
+        EditText searchEditText = (EditText) searchView.findViewById(androidx.appcompat.R.id.search_src_text);
+        searchEditText.setTextColor(Color.BLACK);
+        searchEditText.setHintTextColor(Color.BLACK);
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
@@ -325,7 +293,7 @@ public class general_events extends Fragment implements SwipeRefreshLayout.OnRef
 
                     }
                 }
-                GenAdapter searchAdapter = new GenAdapter(getContext(), filteredEvents, general_events.this::onEventClick);
+                GenAdapter searchAdapter = new GenAdapter(getContext(), filteredEvents, general_events.this);
                 recyclerView.setAdapter(searchAdapter);
                 return false;
             }
@@ -346,7 +314,7 @@ public class general_events extends Fragment implements SwipeRefreshLayout.OnRef
             }
 
         }
-        GenAdapter filterAdapter = new GenAdapter(getContext(), Fevents, this::onEventClick);
+        GenAdapter filterAdapter = new GenAdapter(getContext(), Fevents, this);
         recyclerView.setAdapter(filterAdapter);
     }
 
@@ -355,29 +323,30 @@ public class general_events extends Fragment implements SwipeRefreshLayout.OnRef
         Fevents.clear();
         searchView.setQuery("", false);
         searchView.clearFocus();
-        GenAdapter filterAdapter = new GenAdapter(getContext(), eventArrayList, this::onEventClick);
+        GenAdapter filterAdapter = new GenAdapter(getContext(), eventArrayList, this);
         recyclerView.setAdapter(filterAdapter);
     }
 
-    public void PartyFilter(View view) {
+    public void PartyFilter() {
         FilterList("Party");
     }
 
-    public void FoodFilter(View view) {
+    public void FoodFilter() {
         FilterList("Food");
     }
 
-    public void GreekFilter(View view) {
+    public void GreekFilter() {
         FilterList("Greek Life");
     }
 
-    public void TutorFilter(View view) {
+    public void TutorFilter() {
         FilterList("Tutoring");
     }
 
-    public void ClubFilter(View view) {
+    //need for when we implement club filter
+    /*public void ClubFilter() {
         FilterList("Club");
-    }
+    }*/
 
 
     @Override
@@ -392,15 +361,16 @@ public class general_events extends Fragment implements SwipeRefreshLayout.OnRef
 
     }
 
-
-    //Todo: work on refreshing the page next
-
     @Override
     public void onRefresh() {
 
         swipeRefreshLayout.setRefreshing(false);
     }
 
+
+
+
+    // method for making events clickable and sending data to the more info fragment
     @Override
     public void onEventClick(int position) {
         Log.d(TAG, "onEventClick: clicked ");
